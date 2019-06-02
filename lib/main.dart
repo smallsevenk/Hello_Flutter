673fecc1919d4a77ec163d5b0badbef12315e191
淡然 <smallsevenk@vip.qq.com>
2019-06-02 09:32:18 +0800

    import

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          //child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
